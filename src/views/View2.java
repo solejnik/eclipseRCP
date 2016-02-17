3fff8b215195cecf0ef48ede25a8c082d251ba13
11cervantes11 <solejnik@CE09507.corp.capgemini.com>
2016-02-17 13:03:38 +0100

    almost all done

--- src/views/View2.java
+++ src/views/View2.java
@@ -12,31 +12,75 @@ import org.eclipse.ui.part.ViewPart;
 
 import book.Book;
 import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class View2 extends ViewPart {
 	public View2() {
 	}
+	private boolean update;
+	private String authorFirstName;
+	private String authorLastName;
 	public static final String ID = "BookProject.view";
 	public void createPartControl(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
-		
-		Label lblAutor = new Label(parent, SWT.NONE);
-		lblAutor.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
+		parent.setLayout(new GridLayout(3, false));
+		Label lblAutor,lblNewLabel,lblNewLabel_1,lblNewLabel_2,lblNewLabel_3;
+		lblAutor = new Label(parent, SWT.NONE);
+		lblAutor.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.BOLD));
 		lblAutor.setText("Autor");
+		new Label(parent, SWT.NONE);
 		
-		Label lblNewLabel = new Label(parent, SWT.NONE);
-		lblNewLabel.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.BOLD));
-		lblNewLabel.setText("");
+		Label lblUpdateType = new Label(parent, SWT.NONE);
+		lblUpdateType.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.BOLD));
+		lblUpdateType.setText("Update type");
 		
-		Label lblNewLabel_1 = new Label(parent, SWT.NONE);
-		lblNewLabel_1.setText("New Label");
+		lblNewLabel = new Label(parent, SWT.NONE);
+		lblNewLabel.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
+		lblNewLabel.setText("First name");
+		new Label(parent, SWT.NONE);
 		
-		Label lblNewLabel_2 = new Label(parent, SWT.NONE);
-		lblNewLabel_2.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.BOLD));
-		lblNewLabel_2.setText("");
+		Button btnDynamically = new Button(parent, SWT.RADIO);
+		btnDynamically.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				update = true;
+			}
+		});
+		btnDynamically.setText("Dynamically");
 		
-		Label lblNewLabel_3 = new Label(parent, SWT.NONE);
-		lblNewLabel_3.setText("New Label");
+		lblNewLabel_1 = new Label(parent, SWT.NONE);
+		lblNewLabel_1.setText("                    ");
+		new Label(parent, SWT.NONE);
+		
+		Button btnManually = new Button(parent, SWT.RADIO);
+		btnManually.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				update = false;
+			}
+		});
+		btnManually.setText("Manually");
+		
+		lblNewLabel_2 = new Label(parent, SWT.NONE);
+		lblNewLabel_2.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
+		lblNewLabel_2.setText("Last name");
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		
+		
+		lblNewLabel_3 = new Label(parent, SWT.NONE);
+		lblNewLabel_3.setText("                    ");
+		new Label(parent, SWT.NONE);
+		Button btnUpdate = new Button(parent, SWT.NONE);
+		btnUpdate.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				lblNewLabel_1.setText(authorFirstName);
+				lblNewLabel_3.setText(authorLastName);
+			}
+		});
+		btnUpdate.setText("Update");
 		
 		getSite().getPage().addSelectionListener(new ISelectionListener() {
 			@Override
@@ -46,8 +90,12 @@ public class View2 extends ViewPart {
 					Object o = strucSelection.getFirstElement();
 					if (o instanceof Book) {
 						Book element = (Book) o;
-						lblNewLabel.setText(element.getAuthor().getFirstName());
-						lblNewLabel_2.setText(element.getAuthor().getLastName());
+						authorFirstName = element.getAuthor().getFirstName();
+						authorLastName = element.getAuthor().getLastName();
+						if(update){
+							lblNewLabel_1.setText(authorFirstName);
+							lblNewLabel_3.setText(authorLastName);
+						}
 					}
 				}
 			}
